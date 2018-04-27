c3d9ce6ac520c41227e306add70b987e92b7116f
acenter2507 <acenter2507@gmail.com>
2018-04-27 15:56:04 +0900

    Commited from lenh at Fri, Apr 27, 2018  3:56:04 PM

--- modules/core/client/config/core-admin.client.menus.js
+++ modules/core/client/config/core-admin.client.menus.js
@@ -2,11 +2,5 @@
 
 angular.module('core.admin').run(['Menus',
   function (Menus) {
-    Menus.addMenuItem('topbar', {
-      title: 'Admin',
-      state: 'admin',
-      type: 'dropdown',
-      roles: ['admin']
-    });
   }
 ]);
