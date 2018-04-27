192b3453b663164c324593c4beb0f0a169291cf2
acenter2507 <acenter2507@gmail.com>
2018-04-27 17:06:18 +0900

    Commited from lenh at Fri, Apr 27, 2018  5:06:18 PM

--- modules/products/client/controllers/products.client.controller.js
+++ modules/products/client/controllers/products.client.controller.js
@@ -122,5 +122,15 @@
         $scope.handleShowToast(res.data.message, true);
       }
     }
+    // Change image from URI to blob
+    function dataURItoBlob(dataURI) {
+      var binary = atob(dataURI.split(',')[1]);
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+      var array = [];
+      for (var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+      }
+      return new Blob([new Uint8Array(array)], { type: mimeString });
+    }
   }
 }());
