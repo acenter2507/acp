286210fbaf06f547df9b7ef91eb061f35c12c0f7
acenter2507 <acenter2507@gmail.com>
2018-04-27 14:34:31 +0900

    Commited from lenh at Fri, Apr 27, 2018  2:34:31 PM

--- modules/combos/client/config/combos.client.config.js
+++ modules/combos/client/config/combos.client.config.js
@@ -5,7 +5,7 @@
     .module('combos')
     .run(menuConfig);
 
-  menuConfig.$inject = ['menuService'];
+  menuConfig.$inject = ['Menus'];
 
   function menuConfig(menuService) {
     // Set top bar menu items
