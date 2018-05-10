1c37947b3b22ba131695c0578510adf53cd58bfd
acenter2507 <acenter2507@gmail.com>
2018-05-10 09:35:31 +0900

    Commited from lenh at Thu, May 10, 2018  9:35:30 AM

---
+++ config/lib/job.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var CronJob = require('cron').CronJob,
+  rank_user = require('../jobs/rank-user');
+
+
+var test_job = new CronJob({
+  cronTime: '* * * * *',
+  onTick: function() {
+    // rank_user.excute();
+  },
+  start: false,
+  timeZone: 'Asia/Tokyo'
+});
+var rank_user_job = new CronJob({
+  cronTime: '0 0 * * *', //every 24hours (every midnight)
+  onTick: function() {
+    rank_user.excute();
+  },
+  start: false,
+  timeZone: 'Asia/Tokyo'
+});
+function start() {
+  rank_user_job.start();
+}
+exports.start = start;
